4e3f39b184688de9ba55e2838d0fe85f43b19152
fabriciobischoff <fabriciobischoff27@gmail.com>
2023-03-31 17:00:42 -0300

    Create aula01-tipos-primitivos.py
    Finalizando primeira aula de tipos primitivos

---
+++ aula01-tipos-primitivos.py
@@ -0,0 +1,8 @@
+# Tipos primitivos, int, float, bool, str
+# print(type(n1)) - descobre o tipo
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+#print('A soma entre5', n1, 'e', n2, 'vale', s)
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
