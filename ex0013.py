7ae2f74cc51260c8c465ae986ab475c25cfd1799
fabriciobischoff <fabriciobischoff27@gmail.com>
2023-04-01 09:42:18 -0300

    Create ex0013.py
    Finalizando exercicio 0013.

---
+++ ex0013.py
@@ -0,0 +1,4 @@
+print('==== DESAFIO 0013 ====')
+salario = float(input('Informe o salário do funcionário R$: '))
+
+print('Com 15% de aumento seu novo salário é de R$ {}'.format(salario + ((15 / 100) * salario)))
