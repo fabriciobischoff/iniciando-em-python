67bfa291f61d4f90b38917c2343fcac29fa8c7da
fabriciobischoff <fabriciobischoff27@gmail.com>
2023-04-01 08:33:39 -0300

    Create ex005.py
    Finalizando exercicio 005.

---
+++ ex005.py
@@ -0,0 +1,6 @@
+print('==== DESAFIO 005 ====')
+numero = int(input('Digite um número:'))
+
+print('O antecessor de {} é {}'.format(numero, numero - 1))
+print('O sucessor de {} é {}'.format(numero, numero + 1))
+
