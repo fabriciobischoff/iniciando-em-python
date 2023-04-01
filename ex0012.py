5091f3804ad18c3ba0758e347e7e64e81c9940dd
fabriciobischoff <fabriciobischoff27@gmail.com>
2023-04-01 09:36:25 -0300

    Create ex0012.py
    Finalizando exercicio 0012

---
+++ ex0012.py
@@ -0,0 +1,4 @@
+print('==== DESAFIO 0012 ====')
+valorProd = float(input('Informe o valor do produto R$ '))
+
+print('Com 5% de desconto agora custa R$ {}'.format(valorProd - ((5 / 100) * valorProd)))
