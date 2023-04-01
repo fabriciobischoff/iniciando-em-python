f859ade7c436f91bb1a9b5aa4097bc0d88ace2ac
fabriciobischoff <fabriciobischoff27@gmail.com>
2023-04-01 08:46:48 -0300

    Create ex007.py
    Finalizando exercicio 007.

---
+++ ex007.py
@@ -0,0 +1,5 @@
+print('==== DESAFIO 007 ====')
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a outra nota: '))
+
+print('A médida das notas = {:.2f}'.format((nota1 + nota2) / 2))
