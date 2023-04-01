a3a866591fde1bce871228a4db84a1e1824c84fd
fabriciobischoff <fabriciobischoff27@gmail.com>
2023-04-01 08:41:44 -0300

    Create ex006.py
    Finalizando exercicio 006.

---
+++ ex006.py
@@ -0,0 +1,6 @@
+print('==== DESAFIO 006 ====')
+numero = float(input('Digite um número: '))
+
+print('O dobro de {} é {}'.format(numero, numero * 2))
+print('O triplo de {} é {}'.format(numero, numero * 3))
+print('A raiz quadrada de {} é {}'.format(numero, numero ** (1/2))) # potencia dele por meio
