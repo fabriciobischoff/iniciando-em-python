ef803bc496369cf3fd3a7e3e297631fa67f1b79e
fabriciobischoff <fabriciobischoff27@gmail.com>
2023-04-01 08:53:56 -0300

    Create ex008.py
    Finalizando exercicio 008.

---
+++ ex008.py
@@ -0,0 +1,5 @@
+print('==== DESAFIO 008 ====')
+valor = float(input('Informe o valor em metros quadrados: '))
+
+print('{} metro(s) em centimetros equivale a: {}'.format(valor, valor * 100), 'cm')
+print('{} metro(s) em milímetros equivale a: {}'.format(valor, valor * 1000), 'mm')
